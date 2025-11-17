d9f653b5972f9a80f03ed092302e448eb923d010
Nathaniel <everalert@users.noreply.github.com>
2025-11-17 20:32:25 +1100

    refactor: move console stuff into its own sub-project

--- build.bat
+++ build.bat
@@ -3,7 +3,8 @@
 set "i=%1"
 
 if "%i%"=="000" set d=000-template & goto :build
-if "%i%"=="001" set d=001-createwindow & goto :build
+if "%i%"=="001" set d=001-console & goto :build
+if "%i%"=="002" set d=002-window & goto :build
 echo sub-project not found
 goto :end
 
